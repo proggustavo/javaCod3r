22cc0db3c15b2a85df2f52e766e148e4fb9169a7
proggustavo <prog.gustavosantos@gmail.com>
2020-06-01 11:59:46 -0300

    Opção de get usando o requestParam

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.cod3r.exerciciossb.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exerciciossb.models.Cliente;
@@ -16,9 +17,14 @@ public class ClienteController {
 		return new Cliente(28, "Pedro", "096.309.859-42");
 	}
 	
-	@GetMapping("/{id}") // as chaves e a notation PathVariable indicam que é um variável
+	@GetMapping("/{id}") // as chaves e a notation PathVariable indicam que é uma variável
 	public Cliente obterClientePorId1(@PathVariable int id) { 
 		return new Cliente(id, "Maria", "987.654.321-00");
 		
+	}
+	
+	@GetMapping // modelo de url que é mais usado pelos serviços /?id=112
+	public Cliente obterClientePorId2(@RequestParam(name="id", defaultValue = "1") int id) {
+		return new Cliente(id, "João Augusto", "111.222.333-44");
 	}
 }
